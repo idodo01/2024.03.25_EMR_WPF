eb207441e9e30ec930d2616027afb0b6c7337dbd
BYUNSUJUNG <susususu07@naver.com>
2025-03-25 23:00:37 +0900

    git init 2025-03-25

---
+++ Views/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using EMR.ViewModels.Pages;
+using Wpf.Ui.Controls;
+
+namespace EMR.Views.Pages
+{
+    public partial class SettingsPage : INavigableView<SettingsViewModel>
+    {
+        public SettingsViewModel ViewModel { get; }
+
+        public SettingsPage(SettingsViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+
+            InitializeComponent();
+        }
+    }
+}
